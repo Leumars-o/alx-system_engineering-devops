9d313b8cdceb29a3e8ef431d96a485596d5cc756
leumars-o <samex150@gmail.com>
2024-02-28 04:57:45 +0100

    task-3: repititive token #2

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,4 +2,4 @@
 # a ruby script that aacepts one arguemnt and
 # matches a regex expression
 
-puts ARGV[0].scan(/hbt{2, 5}n/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# a ruby script that matches a regex for a repititve token
+
+puts ARGV[0].scan(/hbt+n/).join
