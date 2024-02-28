4d34e78e4d81ce45502ccb3a0a6c085c9759a7ae
leumars-o <samex150@gmail.com>
2024-02-28 06:00:46 +0100

    task-8: documented

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# a ruby script that matches a regex for a repititve token
+# a ruby script that matches a regex for [SENDER],[RECEIVER],[FLAGS]
 
 puts ARGV[0].scan(/\[from:([\+?0-9A-Za-z]+)\] \[to:(\+?\d+)\] \[flags:(-?\d+:-?\d+:-?\d+:-?\d+:-?\d+)\]/).join(",")
