a3b17bf82713f6a8554ec6e8889ad3077489c1cc
leumars-o <samex150@gmail.com>
2024-04-22 05:55:29 +0100

    client puppet config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# set up your client SSH configuration file using puppet
+
+exec {'/etc/ssh/ssh_config':
+  path    => '/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school:wq" >> /etc/ssh/ssh_config',
+}
